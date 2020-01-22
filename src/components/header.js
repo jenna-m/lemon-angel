50b3830b689aea0be6370d89fa09ca3e8fe2288f
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-22 18:16:24 -0500

    Update header.js
    Add links

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 class Header extends React.Component {
 	render() {
 		return (
 			<div className="header-container">
-				<h1>Header</h1>
+				<nav>
+					<NavLink to="#about" activeClassName="selected" smooth>
+						About
+					</NavLink>
+					<NavLink to="#portfolio" activeClassName="selected" smooth>
+						Portfolio
+					</NavLink>
+					<NavLink to="#contact" activeClassName="selected" smooth>
+						Contact
+					</NavLink>
+				</nav>
 			</div>
 		);
 	}
