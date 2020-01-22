39457afda858a34a8b89fdc20fdc9bbb097b355b
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-22 17:15:47 -0500

    Create components
    Header, Landing, About, Portfolio, and Contact

---
+++ src/components/portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Portfolio extends React.Component {
+	render() {
+		return (
+			<div className="portfolio-container">
+				<h1>Portfolio</h1>
+			</div>
+		);
+	}
+}
+
+export default Portfolio;
