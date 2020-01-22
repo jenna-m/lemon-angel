54af3a302a67600f1b963b30a4a6023826e200be
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-22 17:16:01 -0500

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './App.scss';
+// Import components
+import Header from './components/header';
+import Landing from './components/landing';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
 
-function App() {
-	return <div className="App"></div>;
+class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<Header />
+				<Landing />
+				<About />
+				<Portfolio />
+				<Contact />
+				<div className="footer">
+					© Elizabeth Garcia 2020 | Web Design by{' '}
+					<a
+						href="https://jennamichaels.dev"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Jenna Michaels
+					</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
