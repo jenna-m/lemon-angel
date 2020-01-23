b3d340e8ec722f0566a1819e8a3d68e2a5ad9afb
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-22 19:24:08 -0500

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import aboutImg from '../images/aboutImg.jpg';
 
 class About extends React.Component {
 	render() {
 		return (
 			<div className="about-container">
 				<h1>About</h1>
+				<img
+					src={aboutImg}
+					alt="Elizabeth Garcia in a chef's uniform"
+				/>
+				<p>
+					Elizabeth García, <span>CCSF Alumna</span>, is a{' '}
+					<span>pastry chef</span> currently located in{' '}
+					<span>San Diego, CA</span>.
+				</p>
 			</div>
 		);
 	}
