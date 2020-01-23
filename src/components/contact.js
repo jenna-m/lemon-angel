8a29a406e84be5cd9a8802d9245185b72e0510d6
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-22 19:24:11 -0500

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import contactImg from '../images/contactImg.jpg';
 
 class Contact extends React.Component {
 	render() {
 		return (
 			<div className="contact-container">
 				<h1>Contact</h1>
+				<img
+					src={contactImg}
+					alt="Elizabeth forming dough into loaves"
+				/>
+				<p>hello@lemonangel.net</p>
 			</div>
 		);
 	}
