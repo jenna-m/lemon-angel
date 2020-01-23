c9ecdb755b01fe9308625a527a57531775518c9f
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-23 18:18:37 -0500

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,13 +5,17 @@ class Contact extends React.Component {
 	render() {
 		return (
 			<div className="contact-container">
-				<img
-					className="section-img"
-					id="contact-img"
-					src={contactImg}
-					alt="Elizabeth forming dough into loaves"
-				/>
-				<p>hello@lemonangel.net</p>
+				<div>
+					<img
+						className="section-img"
+						id="contact-img"
+						src={contactImg}
+						alt="Elizabeth forming dough into loaves"
+					/>
+				</div>
+				<div>
+					<p>hello@lemonangel.net</p>
+				</div>
 			</div>
 		);
 	}
