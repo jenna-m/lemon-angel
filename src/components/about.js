9282f9b0b1064bdfa5440a97b6394a1b1cd955e9
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-23 17:33:18 -0500

    Remove headers

--- src/components/about.js
+++ src/components/about.js
@@ -5,21 +5,16 @@ class About extends React.Component {
 	render() {
 		return (
 			<div className="about-container">
-				<div className="section-title">
-					<h1>About</h1>
-				</div>
-				<div className="section-content">
-					<img
-						className="section-img"
-						id="about-img"
-						src={aboutImg}
-						alt="Elizabeth Garcia in a chef's uniform"
-					/>
-					<p>
-						Elizabeth García, CCSF Alumna, is a pastry chef based in
-						San Diego, CA.
-					</p>
-				</div>
+				<img
+					className="section-img"
+					id="about-img"
+					src={aboutImg}
+					alt="Elizabeth Garcia in a chef's uniform"
+				/>
+				<p>
+					Elizabeth García, CCSF Alumna, is a pastry chef based in San
+					Diego, CA.
+				</p>
 			</div>
 		);
 	}
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,17 +4,12 @@ class Portfolio extends React.Component {
 	render() {
 		return (
 			<div className="portfolio-container">
-				<div className="section-title">
-					<h1>Portfolio</h1>
-				</div>
-				<div className="portfolio-gallery">
-					<p>Image 1</p>
-					<p>Image 2</p>
-					<p>Image 3</p>
-					<p>Image 4</p>
-					<p>Image 5</p>
-					<p>Image 6</p>
-				</div>
+				<p>Image 1</p>
+				<p>Image 2</p>
+				<p>Image 3</p>
+				<p>Image 4</p>
+				<p>Image 5</p>
+				<p>Image 6</p>
 			</div>
 		);
 	}
