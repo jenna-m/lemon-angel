56274a2c9a883abc697dd30d74e158f3db4aca76
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-23 18:18:44 -0500

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -5,16 +5,20 @@ class About extends React.Component {
 	render() {
 		return (
 			<div className="about-container">
-				<img
-					className="section-img"
-					id="about-img"
-					src={aboutImg}
-					alt="Elizabeth Garcia in a chef's uniform"
-				/>
-				<p>
-					Elizabeth García, CCSF Alumna, is a pastry chef based in San
-					Diego, CA.
-				</p>
+				<div>
+					<img
+						className="section-img"
+						id="about-img"
+						src={aboutImg}
+						alt="Elizabeth Garcia in a chef's uniform"
+					/>
+				</div>
+				<div>
+					<p>
+						Elizabeth García, CCSF Alumna, is a pastry chef based in
+						San Diego, CA.
+					</p>
+				</div>
 			</div>
 		);
 	}
