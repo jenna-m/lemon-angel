a3b8fb06b619e3a9f2021cb27fb72dc6dd695e53
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-23 18:18:13 -0500

    Add images to portfolio component

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,15 +1,34 @@
 import React from 'react';
+// import images
+import img1 from '../images/landing/1.jpg';
+import img2 from '../images/landing/2.jpg';
+import img3 from '../images/landing/3.jpg';
+import img4 from '../images/landing/4.jpg';
+import img5 from '../images/landing/5.jpg';
 
 class Portfolio extends React.Component {
 	render() {
 		return (
 			<div className="portfolio-container">
-				<p>Image 1</p>
-				<p>Image 2</p>
-				<p>Image 3</p>
-				<p>Image 4</p>
-				<p>Image 5</p>
-				<p>Image 6</p>
+				<div className="portfolio-img-container">
+					<img src={img1} alt="Carrot Cakes" />
+					<img
+						src={img2}
+						alt="Chocolate Cake with Floral Decoration"
+					/>
+					<img
+						src={img3}
+						alt="Strawberry on Top of Decorated Cupcake"
+					/>
+					<img
+						src={img4}
+						alt="Cake with Edible Decorative Grass and Strawberries"
+					/>
+					<img
+						src={img5}
+						alt="Cheesecake Brownies with Colorful Sprinkles"
+					/>
+				</div>
 			</div>
 		);
 	}
