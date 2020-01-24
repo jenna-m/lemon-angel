7664caeef520308717fb3aa2b01c151c0ef4f3a8
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-24 14:46:39 -0500

    Implement hidden on scroll navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import classnames from 'classnames';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 class Header extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			prevScrollPosition: window.pageYOffset,
+			visible: true
+		};
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll);
+	}
+
+	// method to hide/show navbar on scroll down/up
+	handleScroll = () => {
+		const { prevScrollPosition } = this.state;
+
+		const currentScrollPosition = window.pageYOffset;
+		const visible = prevScrollPosition > currentScrollPosition;
+
+		this.setState({
+			prevScrollPosition: currentScrollPosition,
+			visible
+		});
+	};
+
 	render() {
 		return (
 			<div className="header-container">
-				<nav>
+				<nav
+					className={classnames('navbar', {
+						'navbar--hidden': !this.state.visible
+					})}
+				>
+					{' '}
 					<NavLink to="#about" activeClassName="selected" smooth>
 						About
 					</NavLink>
