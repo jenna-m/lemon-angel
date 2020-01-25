76bb8de0ca85533179b0ef75fa19697a168b0c7f
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-25 14:40:49 -0500

    Add more portfolio images

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -12,11 +12,16 @@ import img9 from '../images/portfolio/9.jpg';
 import img10 from '../images/portfolio/10.jpg';
 import img11 from '../images/portfolio/11.jpg';
 import img12 from '../images/portfolio/12.jpg';
+import img13 from '../images/portfolio/13.jpg';
+import img14 from '../images/portfolio/14.jpg';
+import img15 from '../images/portfolio/15.jpg';
+import img16 from '../images/portfolio/16.jpg';
 
 class Portfolio extends React.Component {
 	render() {
 		return (
 			<div className="portfolio-container">
+				<h1 className="accessible-header">Portfolio</h1>
 				<div className="portfolio-img-container">
 					<img
 						className="flex-item"
@@ -45,11 +50,6 @@ class Portfolio extends React.Component {
 					/>
 					<img
 						className="flex-item"
-						src={img6}
-						alt="Plate of Fig and Cheese Danishes"
-					/>
-					<img
-						className="flex-item"
 						src={img7}
 						alt="Unbaked Single-Serving Sized Baked Alaskas"
 					/>
@@ -57,6 +57,11 @@ class Portfolio extends React.Component {
 						className="flex-item"
 						src={img8}
 						alt="Torched Meringue Swirl on Top of a Pie"
+					/>
+					<img
+						className="flex-item"
+						src={img6}
+						alt="Plate of Fig and Cheese Danishes"
 					/>
 					<img
 						className="flex-item"
@@ -77,6 +82,26 @@ class Portfolio extends React.Component {
 						className="flex-item"
 						src={img12}
 						alt="Cheesecake Brownies with Colorful Sprinkles"
+					/>
+					<img
+						className="flex-item"
+						src={img13}
+						alt="Vanilla Bean Mascarpone Mousse, Macerated Rhubarb, Raspberry Sorbet With Lemon Verbena, Lavender Meringue and Candied Rose Petal Garnish"
+					/>
+					<img
+						className="flex-item"
+						src={img14}
+						alt="Heart-Shaped Flan with Black Sesame Seed Topping"
+					/>
+					<img
+						className="flex-item"
+						src={img15}
+						alt="Cupcakes with Frosted Flower Decorations"
+					/>
+					<img
+						className="flex-item"
+						src={img16}
+						alt="Chocolate Cupcakes with Frosting Grass and Fondant Bunnies with Their Butts in the Air"
 					/>
 				</div>
 			</div>
